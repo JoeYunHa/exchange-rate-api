50894f9967feae6df133cfe159667d0941e89bd0
Joe <jojojo7391@gmail.com>
2025-12-04 15:46:03 +0900

    index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
 // 서버 진입점
+// Express 서버와 Apollo Server 결합 + MongoDB에 연결 => server 시작
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// import schema, resolver
+import typeDefs from "./schema/typeDefs";
+import resolvers from "./resolvers/index";
+
+dotenv.config(); // 환경변수 로드
+
+const app = express();
+// Express 앱을 감싸는 HTTP 서버 생성
+// -> Apollo의 grace terminate를 위함
+const httpServer = http.createServer(app);
+
+// Apollo Server instance 생성
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  pulgins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+// (async)Apollo Server 시작
+await server.start();
+
+// middleware : /graphql -> Apollo가 처리
+app.use("/graphql", cors(), bodyParser.json(), expressMiddleware(server));
+
+const PORT = process.env.PORT || 5110;
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/exchange_db";
+
+// DB 연결 및 서버 실행
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    return new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+  })
+  .then(() => {
+    // 과제 조건
+    console.log(`Server ready at http://localhost:${PORT}/graphql`);
+  })
+  .catch((err) => {
+    console.error("Server Error:", err);
+  });
